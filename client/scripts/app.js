2d7a96564982794f385af690375404b5cd4436e4
jakehsiao0909 <jakehsiao0909@gmail.com>
2018-03-16 17:25:46 -0500

    Render all past messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,68 @@
-// YOUR CODE HERE:
+$(document).ready( function() {
+    app.fetch();
+});
+let app = {
+
+    server: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
+    
+    init: function () {
+        app.renderMessage();
+    },
+
+    getMessage: function() {
+        var output = {
+            username: "DefaultUser",
+            text: $("#text").val(),
+            roomname: $("#room").val(),
+        };
+        app.send(output);
+        app.renderMessages();
+    },
+
+    send: function(message) {
+
+        $.ajax({
+            url: app.server,
+            type: 'POST',
+            data: JSON.stringify(message),
+            contentType: 'application/json',
+            success: function (data) {
+                console.log('chatterbox: Message sent');
+            },
+            error: function (data) {
+                // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+                console.error('chatterbox: Failed to send message', data);
+            }
+        });
+    },
+
+    fetch: function() {
+        $.ajax({
+            url: app.server,
+            type: 'GET',
+            success: function (data) {
+                console.log('chatterbox: Message retrieved');
+                var array = data.results;
+                array.forEach(function(element) {
+                    app.renderMessage(element);
+                });
+            },
+            error: function (data) {
+                // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+                console.error('chatterbox: Failed to retrieve message', data);
+            }
+        }); 
+    },
+
+    clearMessages: function() {
+
+    },
+    renderMessage: function(messages) {
+        if (!messages) return;
+        $('#chats').prepend(`<div class="chat"><p class="username">${messages.username}:</p><p>${messages.text}</p></div>`);
+    },
+
+    renderRoom: function() {
+
+    }
+}
